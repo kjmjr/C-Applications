ee733428dff701f18a71b028b9b1c48c2251e145
Kevin McAdoo <53047422+kjmjr@users.noreply.github.com>
2019-07-23 18:32:07 -0400

    Dice simulator
    In this game I learned how to use the random number generator method to simulate 6 dices whenever the button is clicked. Out of each roll the application will present 2 dice.

---
+++ Dice Simulator/Dice Simulator/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Dice_Simulator
+{
+    partial class frmDiceSimulator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnUser = new System.Windows.Forms.Button();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblUserRoll = new System.Windows.Forms.Label();
+            this.lblComputersRoll = new System.Windows.Forms.Label();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.picComputer2 = new System.Windows.Forms.PictureBox();
+            this.picComputer1 = new System.Windows.Forms.PictureBox();
+            this.picUser2 = new System.Windows.Forms.PictureBox();
+            this.picUser1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picComputer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picComputer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUser2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUser1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(525, 455);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(126, 39);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnUser
+            // 
+            this.btnUser.Location = new System.Drawing.Point(203, 404);
+            this.btnUser.Name = "btnUser";
+            this.btnUser.Size = new System.Drawing.Size(220, 37);
+            this.btnUser.TabIndex = 3;
+            this.btnUser.Text = "Click to Roll Dice";
+            this.btnUser.UseVisualStyleBackColor = true;
+            this.btnUser.Click += new System.EventHandler(this.btnUser_Click);
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(291, 363);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(29, 13);
+            this.lblUser.TabIndex = 5;
+            this.lblUser.Text = "User";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(253, 22);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(106, 13);
+            this.lblTitle.TabIndex = 7;
+            this.lblTitle.Text = "Dice Simulator Game";
+           // this.lblTitle.Click += new System.EventHandler(this.lblTitle_Click);
+            // 
+            // lblUserRoll
+            // 
+            this.lblUserRoll.AutoSize = true;
+            this.lblUserRoll.Location = new System.Drawing.Point(33, 140);
+            this.lblUserRoll.Name = "lblUserRoll";
+            this.lblUserRoll.Size = new System.Drawing.Size(61, 13);
+            this.lblUserRoll.TabIndex = 9;
+            this.lblUserRoll.Text = "User Rolls: ";
+            // 
+            // lblComputersRoll
+            // 
+            this.lblComputersRoll.AutoSize = true;
+            this.lblComputersRoll.Location = new System.Drawing.Point(33, 278);
+            this.lblComputersRoll.Name = "lblComputersRoll";
+            this.lblComputersRoll.Size = new System.Drawing.Size(81, 13);
+            this.lblComputersRoll.TabIndex = 10;
+            this.lblComputersRoll.Text = "Computer Rolls:";
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(203, 474);
+            this.txtResults.Name = "txtResults";
+            this.txtResults.Size = new System.Drawing.Size(220, 20);
+            this.txtResults.TabIndex = 12;
+            // 
+            // picComputer2
+            // 
+            this.picComputer2.Image = global::Dice_Simulator.Properties.Resources.DiceTwo;
+            this.picComputer2.Location = new System.Drawing.Point(368, 240);
+            this.picComputer2.Name = "picComputer2";
+            this.picComputer2.Size = new System.Drawing.Size(105, 91);
+            this.picComputer2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picComputer2.TabIndex = 14;
+            this.picComputer2.TabStop = false;
+            // 
+            // picComputer1
+            // 
+            this.picComputer1.Image = global::Dice_Simulator.Properties.Resources.DiceFour;
+            this.picComputer1.Location = new System.Drawing.Point(139, 240);
+            this.picComputer1.Name = "picComputer1";
+            this.picComputer1.Size = new System.Drawing.Size(104, 91);
+            this.picComputer1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picComputer1.TabIndex = 13;
+            this.picComputer1.TabStop = false;
+            // 
+            // picUser2
+            // 
+            this.picUser2.Image = global::Dice_Simulator.Properties.Resources.DiceThree;
+            this.picUser2.Location = new System.Drawing.Point(368, 89);
+            this.picUser2.Name = "picUser2";
+            this.picUser2.Size = new System.Drawing.Size(105, 106);
+            this.picUser2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picUser2.TabIndex = 2;
+            this.picUser2.TabStop = false;
+            // 
+            // picUser1
+            // 
+            this.picUser1.Image = global::Dice_Simulator.Properties.Resources.DiceFive;
+            this.picUser1.Location = new System.Drawing.Point(139, 89);
+            this.picUser1.Name = "picUser1";
+            this.picUser1.Size = new System.Drawing.Size(104, 103);
+            this.picUser1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picUser1.TabIndex = 1;
+            this.picUser1.TabStop = false;
+            // 
+            // frmDiceSimulator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 534);
+            this.Controls.Add(this.picComputer2);
+            this.Controls.Add(this.picComputer1);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.lblComputersRoll);
+            this.Controls.Add(this.lblUserRoll);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.btnUser);
+            this.Controls.Add(this.picUser2);
+            this.Controls.Add(this.picUser1);
+            this.Controls.Add(this.btnExit);
+            this.Name = "frmDiceSimulator";
+            this.Text = "Dice Simulator Game";
+            ((System.ComponentModel.ISupportInitialize)(this.picComputer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picComputer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUser2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUser1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PictureBox picUser1;
+        private System.Windows.Forms.PictureBox picUser2;
+        private System.Windows.Forms.Button btnUser;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblUserRoll;
+        private System.Windows.Forms.Label lblComputersRoll;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.PictureBox picComputer1;
+        private System.Windows.Forms.PictureBox picComputer2;
+    }
+}
+
---
+++ Dice Simulator/Dice Simulator/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+//Plan: put a picture box with 6 dice pictures
+//use random class to show the picture of the dice each time the roll is complete on the button
+//do while loop to keep playing the game
+//have a user and a computer
+//write results to game_results.txt file that prints the amount of wins by the player and computer
+//and amount of ties
+
+
+namespace Dice_Simulator
+{
+    public partial class frmDiceSimulator : Form
+    {
+        
+
+        public frmDiceSimulator()
+        {
+            InitializeComponent();
+        }
+
+         
+    
+        private void btnUser_Click(object sender, EventArgs e)
+        {
+           
+
+            //variables for keeping track number of wins and ties
+            int computerWins = 0;
+            int userWins = 0;
+            int numberOfTies = 0;
+
+            //total variables
+            int UserTotal = 0;
+            int computerTotal = 0;
+
+           
+                //an object made for the random class
+                Random rand = new Random();
+           
+            //initialize variables
+            int dice1 = new int();
+
+            int dice2 = new int();
+
+            int dice3 = new int();
+
+            int dice4 = new int();                    
+       
+            
+            //if statments for the first dice and the user
+            dice1 = rand.Next(6) + 1;
+            if (dice1 == 0)
+            {
+                picUser1.Image = Properties.Resources.DiceOne;
+                dice1++;
+
+            }
+
+            else if (dice1 == 1)
+            {
+
+                picUser1.Image = Properties.Resources.DiceTwo;
+                dice1++;
+            }
+
+            else if(dice1 == 2)
+            {
+                picUser1.Image = Properties.Resources.DiceThree;
+                dice1++;
+            }
+
+            else if(dice1 == 3)
+            {
+                picUser1.Image = Properties.Resources.DiceFour;
+                dice1++;
+            }
+
+            else if(dice1 == 4)
+            {
+                picUser1.Image = Properties.Resources.DiceFive;
+                dice1++;
+            }
+
+            else 
+            {
+                dice1 = 5;
+                picUser1.Image = Properties.Resources.DiceSix;
+                dice1++;
+            }
+
+
+            //if statments for the secound dice and the user
+            dice2 = rand.Next(6) + 1;
+
+            if (dice2 == 0)
+            {
+                picUser2.Image = Properties.Resources.DiceOne;
+                dice2++;
+            }
+
+            else if(dice2 == 1)
+            {
+                picUser2.Image = Properties.Resources.DiceTwo;
+                dice2++;
+            }
+            
+            else if(dice2 == 2)
+            {
+                picUser2.Image = Properties.Resources.DiceThree;
+                dice2++;
+            }
+
+            else if(dice2 == 3)
+            {
+
+                picUser2.Image = Properties.Resources.DiceFour;
+                dice2++;
+            }
+           
+            else if(dice2 == 4)
+            {
+                picUser2.Image = Properties.Resources.DiceFive;
+                dice2++;
+
+            }
+
+            else 
+            {
+                dice2 = 5;
+                picUser2.Image = Properties.Resources.DiceSix;
+                dice2++;
+
+            }
+
+            dice3 = rand.Next(6) + 1;
+            //if statments for the secound dice and the computer
+            if (dice3 == 0)
+            {
+
+                picComputer1.Image = Properties.Resources.DiceOne;
+                dice3++;
+            }
+
+            else if(dice3 == 1)
+            {
+                picComputer1.Image = Properties.Resources.DiceTwo;
+                dice3++;
+            }
+
+            else if(dice3 == 2)
+            {
+                picComputer1.Image = Properties.Resources.DiceThree;
+                dice3++;
+            }
+
+            else if(dice3 == 3)
+            {
+
+                picComputer1.Image = Properties.Resources.DiceFour;
+                dice3++;
+            }
+
+            else if(dice3 == 4)
+            {
+
+                picComputer1.Image = Properties.Resources.DiceFive;
+                dice3++;
+
+            }
+
+            else 
+            {
+                dice3 = 5;
+                picComputer1.Image = Properties.Resources.DiceSix;
+                dice3++;
+            }
+
+            dice4 = rand.Next(6) + 1;
+            //if statments for the secound dice and the computer
+            if (dice4 == 0)
+            {
+                picComputer2.Image = Properties.Resources.DiceOne;
+                dice4++;
+            }
+
+            else if (dice4 == 1)
+            {
+                picComputer2.Image = Properties.Resources.DiceTwo;
+                dice4++;
+            }
+
+            else if (dice4 == 2)
+            {
+
+                picComputer2.Image = Properties.Resources.DiceThree;
+                dice4++;
+            }
+
+            else if (dice4 == 3)
+            {
+
+                picComputer2.Image = Properties.Resources.DiceFour;
+                dice4++;
+            }
+
+            else if(dice4 == 4)
+            {
+
+                picComputer2.Image = Properties.Resources.DiceFive;
+                dice4++;
+            }
+
+            else
+            {
+                dice4 = 5;
+                picComputer2.Image = Properties.Resources.DiceSix;
+                dice4++;
+            }
+
+            UserTotal = dice1 + dice2;
+            computerTotal = dice3 + dice4;
+
+
+            if (UserTotal > computerTotal)
+            {
+
+                userWins++;
+
+                //display results
+                txtResults.Text = "YOU WIN";
+
+            }
+
+            else if (UserTotal == computerTotal)
+            {
+
+                numberOfTies++;
+                txtResults.Text = "TIE GAME";
+            }
+
+            else if (UserTotal < computerTotal)
+            {
+
+                computerWins++;
+                txtResults.Text = "COMPUTER WINS";
+
+            }//end else
+
+
+            try
+            {
+
+                //StreamWriter is the object for the outputFile
+                StreamWriter outputFile;
+                outputFile = File.CreateText("E:/c#/hw/Dice Simulator/game_results.txt");
+                outputFile.WriteLine();
+                outputFile.WriteLine("Number of wins by user is " + userWins);
+                outputFile.WriteLine("Number of wins by computer is " + computerWins);
+                outputFile.WriteLine("Number of ties is " + numberOfTies);
+                outputFile.WriteLine();
+                outputFile.Close();
+
+
+            }//end try
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }//end catch           
+
+
+        }//end btnUser click event
+
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }//end btnExit click event
+
+    }//end class
+
+}//end namespace
