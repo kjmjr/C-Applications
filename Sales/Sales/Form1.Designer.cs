c2c50d29c34fe170b959bc614f764ed10f3942b3
Kevin McAdoo <53047422+kjmjr@users.noreply.github.com>
2019-07-23 18:58:54 -0400

    Sales
    I used simple math skills to compute the cost of an item while adding its tax percentage to the cost

---
+++ Sales/Sales/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Sales
+{
+    partial class totalSales
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstArray = new System.Windows.Forms.ListBox();
+            this.btnArrayClick = new System.Windows.Forms.Button();
+            this.lblLargest = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.lblSmallest = new System.Windows.Forms.Label();
+            this.txtLargest = new System.Windows.Forms.TextBox();
+            this.txtAverage = new System.Windows.Forms.TextBox();
+            this.txtSmallest = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lstArray
+            // 
+            this.lstArray.FormattingEnabled = true;
+            this.lstArray.Location = new System.Drawing.Point(67, 42);
+            this.lstArray.Name = "lstArray";
+            this.lstArray.Size = new System.Drawing.Size(120, 95);
+            this.lstArray.TabIndex = 0;
+            // 
+            // btnArrayClick
+            // 
+            this.btnArrayClick.Location = new System.Drawing.Point(15, 158);
+            this.btnArrayClick.Name = "btnArrayClick";
+            this.btnArrayClick.Size = new System.Drawing.Size(231, 23);
+            this.btnArrayClick.TabIndex = 1;
+            this.btnArrayClick.Text = "Show and Calculate Array";
+            this.btnArrayClick.UseVisualStyleBackColor = true;
+            this.btnArrayClick.Click += new System.EventHandler(this.btnArrayClick_Click);
+            // 
+            // lblLargest
+            // 
+            this.lblLargest.AutoSize = true;
+            this.lblLargest.Location = new System.Drawing.Point(12, 231);
+            this.lblLargest.Name = "lblLargest";
+            this.lblLargest.Size = new System.Drawing.Size(72, 13);
+            this.lblLargest.TabIndex = 2;
+            this.lblLargest.Text = "Largest Value";
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Location = new System.Drawing.Point(129, 231);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(47, 13);
+            this.lblAverage.TabIndex = 3;
+            this.lblAverage.Text = "Average";
+            // 
+            // lblSmallest
+            // 
+            this.lblSmallest.AutoSize = true;
+            this.lblSmallest.Location = new System.Drawing.Point(216, 231);
+            this.lblSmallest.Name = "lblSmallest";
+            this.lblSmallest.Size = new System.Drawing.Size(76, 13);
+            this.lblSmallest.TabIndex = 4;
+            this.lblSmallest.Text = "Smallest Value";
+            // 
+            // txtLargest
+            // 
+            this.txtLargest.Location = new System.Drawing.Point(15, 261);
+            this.txtLargest.Name = "txtLargest";
+            this.txtLargest.ReadOnly = true;
+            this.txtLargest.Size = new System.Drawing.Size(69, 20);
+            this.txtLargest.TabIndex = 5;
+            // 
+            // txtAverage
+            // 
+            this.txtAverage.Location = new System.Drawing.Point(118, 261);
+            this.txtAverage.Name = "txtAverage";
+            this.txtAverage.ReadOnly = true;
+            this.txtAverage.Size = new System.Drawing.Size(69, 20);
+            this.txtAverage.TabIndex = 6;
+            // 
+            // txtSmallest
+            // 
+            this.txtSmallest.Location = new System.Drawing.Point(219, 261);
+            this.txtSmallest.Name = "txtSmallest";
+            this.txtSmallest.ReadOnly = true;
+            this.txtSmallest.Size = new System.Drawing.Size(73, 20);
+            this.txtSmallest.TabIndex = 7;
+            // 
+            // totalSales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 328);
+            this.Controls.Add(this.txtSmallest);
+            this.Controls.Add(this.txtAverage);
+            this.Controls.Add(this.txtLargest);
+            this.Controls.Add(this.lblSmallest);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.lblLargest);
+            this.Controls.Add(this.btnArrayClick);
+            this.Controls.Add(this.lstArray);
+            this.Name = "totalSales";
+            this.Text = "Total Sales App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstArray;
+        private System.Windows.Forms.Button btnArrayClick;
+        private System.Windows.Forms.Label lblLargest;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Label lblSmallest;
+        private System.Windows.Forms.TextBox txtLargest;
+        private System.Windows.Forms.TextBox txtAverage;
+        private System.Windows.Forms.TextBox txtSmallest;
+    }
+}
+
---
+++ Sales/Sales/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sales
+{
+    public partial class totalSales : Form
+    {
+        public totalSales()
+        {
+            InitializeComponent();
+        }
+
+        private void btnArrayClick_Click(object sender, EventArgs e)
+        {
+            
+          
+            try
+            {
+                //initialize variables
+                const int SIZE = 7;
+                double[] numbersArray = new double[SIZE]; 
+                double largest = 0.0;
+                double smallest = 0;
+                double average = 0.0;
+
+                //calling methods that passed an array
+                ReaderMethod(numbersArray);
+                average = AverageValueMethod(numbersArray);
+                largest = HighestValueMethod(numbersArray);
+                smallest = LowestValueMethod(numbersArray);
+
+                //displaying results
+                txtLargest.Text = largest.ToString("n2");
+                txtSmallest.Text = smallest.ToString("n2");
+                txtAverage.Text = average.ToString("n2");
+
+            }//end try
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message);
+            }//end catch
+
+        }//end btnArray click method
+
+        //passing the declared array into the method to read from a file
+        private void ReaderMethod(double [] numbersArray)
+        {
+
+            //initialize variables
+            //const int SIZE = 7;
+           // double[] numbersArray = new double[SIZE];
+            int i = 0;
+
+            //Pass the file path and file name to the StreamReader constructor
+            System.IO.StreamReader reader = new StreamReader("sales.txt");
+            while (i < numbersArray.Length)
+            {
+                numbersArray[i] = double.Parse(reader.ReadLine());
+                i++;
+
+            }//end while
+             //close the file
+            reader.Close();
+            // Console.ReadLine();
+
+            foreach (double number in numbersArray)
+            {
+                lstArray.Items.Add(number);
+
+            }//end of foreach method
+
+        }//end of ReaderMethod
+
+        private double AverageValueMethod(double [] numbersArray)
+        {
+            //declaring variables
+            double total = 0.0;
+            double average = 0.0;
+           
+
+            for (int i = 0; i < numbersArray.Length; i++)
+            {
+                total += numbersArray[i];
+
+            }
+
+            average = total / numbersArray.Length;
+
+            return average;
+
+        }//end of AverageValueMethod
+
+        private double HighestValueMethod(double[] numbersArray)
+        {
+            double highValue = numbersArray[0];
+           
+
+            for (int i = 0; i < numbersArray.Length; i++)
+            {
+                if (highValue < numbersArray[i])
+                {
+
+                    highValue = numbersArray[i];
+                }
+
+
+            }
+
+            return highValue;
+
+        }//end of HighestValueMethod
+
+        private double LowestValueMethod(double[] numbersArray)
+        {
+            double lowValue = numbersArray[0];
+           
+
+            for (int i = 0; i < numbersArray.Length; i++)
+            {
+                if (lowValue > numbersArray[i])
+                {
+
+                    lowValue = numbersArray[i];
+                }
+
+            }
+
+            return lowValue;
+
+        }//end of LowestValueMethod
+
+
+    }//end class
+}//end namespace
