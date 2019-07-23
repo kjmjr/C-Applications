70ba5664f4b1a23c48f1a9d8bbfaf0053ff9908d
Kevin McAdoo <53047422+kjmjr@users.noreply.github.com>
2019-07-23 19:07:55 -0400

    Color Spectrum

---
+++ Color_Spectrum/Color_Spectrum/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Color_Spectrum
+{
+    partial class colorSpectrumForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSelect = new System.Windows.Forms.Label();
+            this.txtColorBox = new System.Windows.Forms.TextBox();
+            this.redLabel = new System.Windows.Forms.Label();
+            this.orangeLabel = new System.Windows.Forms.Label();
+            this.yellowLabel = new System.Windows.Forms.Label();
+            this.greenLabel = new System.Windows.Forms.Label();
+            this.blueLabel = new System.Windows.Forms.Label();
+            this.indigoLabel = new System.Windows.Forms.Label();
+            this.violetLabel = new System.Windows.Forms.Label();
+            this.lblOrange = new System.Windows.Forms.Label();
+            this.lblRed = new System.Windows.Forms.Label();
+            this.lblYellow = new System.Windows.Forms.Label();
+            this.lblGreen = new System.Windows.Forms.Label();
+            this.lblBlue = new System.Windows.Forms.Label();
+            this.lblIndigo = new System.Windows.Forms.Label();
+            this.lblViolet = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblSelect
+            // 
+            this.lblSelect.AutoSize = true;
+            this.lblSelect.Location = new System.Drawing.Point(296, 86);
+            this.lblSelect.Name = "lblSelect";
+            this.lblSelect.Size = new System.Drawing.Size(151, 13);
+            this.lblSelect.TabIndex = 0;
+            this.lblSelect.Text = "Select a Color of the Spectrum";
+            // 
+            // txtColorBox
+            // 
+            this.txtColorBox.Location = new System.Drawing.Point(299, 362);
+            this.txtColorBox.Name = "txtColorBox";
+            this.txtColorBox.ReadOnly = true;
+            this.txtColorBox.Size = new System.Drawing.Size(199, 20);
+            this.txtColorBox.TabIndex = 1;
+            // 
+            // redLabel
+            // 
+            this.redLabel.AutoSize = true;
+            this.redLabel.Location = new System.Drawing.Point(89, 180);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(0, 13);
+            this.redLabel.TabIndex = 2;
+            // 
+            // orangeLabel
+            // 
+            this.orangeLabel.AutoSize = true;
+            this.orangeLabel.Location = new System.Drawing.Point(166, 180);
+            this.orangeLabel.Name = "orangeLabel";
+            this.orangeLabel.Size = new System.Drawing.Size(0, 13);
+            this.orangeLabel.TabIndex = 3;
+            // 
+            // yellowLabel
+            // 
+            this.yellowLabel.AutoSize = true;
+            this.yellowLabel.Location = new System.Drawing.Point(260, 180);
+            this.yellowLabel.Name = "yellowLabel";
+            this.yellowLabel.Size = new System.Drawing.Size(0, 13);
+            this.yellowLabel.TabIndex = 4;
+            // 
+            // greenLabel
+            // 
+            this.greenLabel.AutoSize = true;
+            this.greenLabel.Location = new System.Drawing.Point(337, 181);
+            this.greenLabel.Name = "greenLabel";
+            this.greenLabel.Size = new System.Drawing.Size(0, 13);
+            this.greenLabel.TabIndex = 5;
+            // 
+            // blueLabel
+            // 
+            this.blueLabel.AutoSize = true;
+            this.blueLabel.Location = new System.Drawing.Point(433, 181);
+            this.blueLabel.Name = "blueLabel";
+            this.blueLabel.Size = new System.Drawing.Size(0, 13);
+            this.blueLabel.TabIndex = 6;
+            // 
+            // indigoLabel
+            // 
+            this.indigoLabel.AutoSize = true;
+            this.indigoLabel.Location = new System.Drawing.Point(540, 181);
+            this.indigoLabel.Name = "indigoLabel";
+            this.indigoLabel.Size = new System.Drawing.Size(0, 13);
+            this.indigoLabel.TabIndex = 7;
+            // 
+            // violetLabel
+            // 
+            this.violetLabel.AutoSize = true;
+            this.violetLabel.Location = new System.Drawing.Point(644, 180);
+            this.violetLabel.Name = "violetLabel";
+            this.violetLabel.Size = new System.Drawing.Size(0, 13);
+            this.violetLabel.TabIndex = 8;
+            // 
+            // lblOrange
+            // 
+            this.lblOrange.AutoSize = true;
+            this.lblOrange.Location = new System.Drawing.Point(284, 132);
+            this.lblOrange.Name = "lblOrange";
+            this.lblOrange.Size = new System.Drawing.Size(42, 13);
+            this.lblOrange.TabIndex = 10;
+            this.lblOrange.Text = "Orange";
+            this.lblOrange.Click += new System.EventHandler(this.lblOrange_Click);
+            // 
+            // lblRed
+            // 
+            this.lblRed.AutoSize = true;
+            this.lblRed.Location = new System.Drawing.Point(157, 132);
+            this.lblRed.Name = "lblRed";
+            this.lblRed.Size = new System.Drawing.Size(27, 13);
+            this.lblRed.TabIndex = 11;
+            this.lblRed.Text = "Red";
+            this.lblRed.Click += new System.EventHandler(this.lblRed_Click);
+            // 
+            // lblYellow
+            // 
+            this.lblYellow.AutoSize = true;
+            this.lblYellow.Location = new System.Drawing.Point(422, 134);
+            this.lblYellow.Name = "lblYellow";
+            this.lblYellow.Size = new System.Drawing.Size(38, 13);
+            this.lblYellow.TabIndex = 12;
+            this.lblYellow.Text = "Yellow";
+            this.lblYellow.Click += new System.EventHandler(this.lblYellow_Click);
+            // 
+            // lblGreen
+            // 
+            this.lblGreen.AutoSize = true;
+            this.lblGreen.Location = new System.Drawing.Point(554, 132);
+            this.lblGreen.Name = "lblGreen";
+            this.lblGreen.Size = new System.Drawing.Size(36, 13);
+            this.lblGreen.TabIndex = 13;
+            this.lblGreen.Text = "Green";
+            this.lblGreen.Click += new System.EventHandler(this.lblGreen_Click);
+            // 
+            // lblBlue
+            // 
+            this.lblBlue.AutoSize = true;
+            this.lblBlue.Location = new System.Drawing.Point(208, 179);
+            this.lblBlue.Name = "lblBlue";
+            this.lblBlue.Size = new System.Drawing.Size(28, 13);
+            this.lblBlue.TabIndex = 14;
+            this.lblBlue.Text = "Blue";
+            this.lblBlue.Click += new System.EventHandler(this.lblBlue_Click);
+            // 
+            // lblIndigo
+            // 
+            this.lblIndigo.AutoSize = true;
+            this.lblIndigo.Location = new System.Drawing.Point(363, 181);
+            this.lblIndigo.Name = "lblIndigo";
+            this.lblIndigo.Size = new System.Drawing.Size(36, 13);
+            this.lblIndigo.TabIndex = 15;
+            this.lblIndigo.Text = "Indigo";
+            this.lblIndigo.Click += new System.EventHandler(this.lblIndigo_Click);
+            // 
+            // lblViolet
+            // 
+            this.lblViolet.AutoSize = true;
+            this.lblViolet.Location = new System.Drawing.Point(499, 181);
+            this.lblViolet.Name = "lblViolet";
+            this.lblViolet.Size = new System.Drawing.Size(33, 13);
+            this.lblViolet.TabIndex = 16;
+            this.lblViolet.Text = "Violet";
+            this.lblViolet.Click += new System.EventHandler(this.lblViolet_Click);
+            // 
+            // colorSpectrumForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblViolet);
+            this.Controls.Add(this.lblIndigo);
+            this.Controls.Add(this.lblBlue);
+            this.Controls.Add(this.lblGreen);
+            this.Controls.Add(this.lblYellow);
+            this.Controls.Add(this.lblRed);
+            this.Controls.Add(this.lblOrange);
+            this.Controls.Add(this.violetLabel);
+            this.Controls.Add(this.indigoLabel);
+            this.Controls.Add(this.blueLabel);
+            this.Controls.Add(this.greenLabel);
+            this.Controls.Add(this.yellowLabel);
+            this.Controls.Add(this.orangeLabel);
+            this.Controls.Add(this.redLabel);
+            this.Controls.Add(this.txtColorBox);
+            this.Controls.Add(this.lblSelect);
+            this.Name = "colorSpectrumForm";
+            this.Load += new System.EventHandler(this.colorSpectrumForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSelect;
+        private System.Windows.Forms.TextBox txtColorBox;
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.Label orangeLabel;
+        private System.Windows.Forms.Label yellowLabel;
+        private System.Windows.Forms.Label greenLabel;
+        private System.Windows.Forms.Label blueLabel;
+        private System.Windows.Forms.Label indigoLabel;
+        private System.Windows.Forms.Label violetLabel;
+        private System.Windows.Forms.Label lblOrange;
+        private System.Windows.Forms.Label lblRed;
+        private System.Windows.Forms.Label lblYellow;
+        private System.Windows.Forms.Label lblGreen;
+        private System.Windows.Forms.Label lblBlue;
+        private System.Windows.Forms.Label lblIndigo;
+        private System.Windows.Forms.Label lblViolet;
+    }
+}
+
---
+++ Color_Spectrum/Color_Spectrum/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Color_Spectrum
+{
+
+    enum Spectrum
+    {
+        Red, Orange, Yellow, Green, Blue, Indigo, Violet
+
+    }//end enum
+    public partial class colorSpectrumForm : Form
+    {
+        public colorSpectrumForm()
+        {
+            InitializeComponent();
+        }
+
+        private void colorSpectrumForm_Load(object sender, EventArgs e)
+        {
+
+        }//end void method
+
+        private void DisplayColor(Spectrum color)
+        {
+            txtColorBox.Text = color.ToString();
+
+        }//end void DisplayColor method
+
+        private void lblRed_Click(object sender, EventArgs e)
+        {
+            DisplayColor(Spectrum.Red);
+        }
+
+        private void lblOrange_Click(object sender, EventArgs e)
+        {
+            DisplayColor(Spectrum.Orange);
+        }
+
+        private void lblYellow_Click(object sender, EventArgs e)
+        {
+            DisplayColor(Spectrum.Yellow);
+        }
+
+        private void lblGreen_Click(object sender, EventArgs e)
+        {
+            DisplayColor(Spectrum.Green);
+        }
+
+        private void lblBlue_Click(object sender, EventArgs e)
+        {
+            DisplayColor(Spectrum.Blue);
+        }
+
+        private void lblIndigo_Click(object sender, EventArgs e)
+        {
+            DisplayColor(Spectrum.Indigo);
+        }
+
+        private void lblViolet_Click(object sender, EventArgs e)
+        {
+            DisplayColor(Spectrum.Violet);
+        }
+    }//end class
+}//end namespace
