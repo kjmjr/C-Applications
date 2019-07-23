e430a6ffd4427f03b8644cc190e9145757f3109d
Kevin McAdoo <53047422+kjmjr@users.noreply.github.com>
2019-07-22 20:46:23 -0400

    Miles per Gallons application
    This project demonstrates how users can covert there input from miles to gallons whenever they click the button the action performs

---
+++ MilesPerGallon/MilesPerGallon/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace MilesPerGallon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMiles = new System.Windows.Forms.Label();
+            this.txtMiles = new System.Windows.Forms.TextBox();
+            this.lblGallons = new System.Windows.Forms.Label();
+            this.txtGallons = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtMpg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblMiles
+            // 
+            this.lblMiles.AutoSize = true;
+            this.lblMiles.Location = new System.Drawing.Point(52, 36);
+            this.lblMiles.Name = "lblMiles";
+            this.lblMiles.Size = new System.Drawing.Size(104, 13);
+            this.lblMiles.TabIndex = 0;
+            this.lblMiles.Text = "Enter Miles Traveled";
+            // 
+            // txtMiles
+            // 
+            this.txtMiles.Location = new System.Drawing.Point(233, 38);
+            this.txtMiles.Name = "txtMiles";
+            this.txtMiles.Size = new System.Drawing.Size(100, 20);
+            this.txtMiles.TabIndex = 1;
+            // 
+            // lblGallons
+            // 
+            this.lblGallons.AutoSize = true;
+            this.lblGallons.Location = new System.Drawing.Point(62, 102);
+            this.lblGallons.Name = "lblGallons";
+            this.lblGallons.Size = new System.Drawing.Size(70, 13);
+            this.lblGallons.TabIndex = 2;
+            this.lblGallons.Text = "Gallons Used";
+            // 
+            // txtGallons
+            // 
+            this.txtGallons.Location = new System.Drawing.Point(235, 97);
+            this.txtGallons.Name = "txtGallons";
+            this.txtGallons.Size = new System.Drawing.Size(100, 20);
+            this.txtGallons.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(130, 207);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(264, 207);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtMpg
+            // 
+            this.txtMpg.Location = new System.Drawing.Point(130, 297);
+            this.txtMpg.Name = "txtMpg";
+            this.txtMpg.ReadOnly = true;
+            this.txtMpg.Size = new System.Drawing.Size(209, 20);
+            this.txtMpg.TabIndex = 6;
+            this.txtMpg.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 366);
+            this.Controls.Add(this.txtMpg);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtGallons);
+            this.Controls.Add(this.lblGallons);
+            this.Controls.Add(this.txtMiles);
+            this.Controls.Add(this.lblMiles);
+            this.Name = "Form1";
+            this.Text = "Miles Per Gallon Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMiles;
+        private System.Windows.Forms.TextBox txtMiles;
+        private System.Windows.Forms.Label lblGallons;
+        private System.Windows.Forms.TextBox txtGallons;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtMpg;
+    }
+}
+
---
+++ MilesPerGallon/MilesPerGallon/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MilesPerGallon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                //declare vars
+                double miles = 0;
+                double gallons = 0;
+                double mpg = 0;
+
+
+
+                //get inputs - miles and gallons
+                miles = double.Parse(txtMiles.Text);
+                gallons = double.Parse(txtGallons.Text);
+
+                //calculate
+                mpg = miles / gallons;
+
+                //display answer
+                txtMpg.Text = mpg.ToString("n2");
+
+            }//end try statement
+
+            catch
+            {
+                MessageBox.Show("INVLAID DATA WAS ENTERED. PLEASE ENTER AGAIN.");
+
+                //sets the text box back to blank after the message
+                txtMiles.Text = "";
+                txtGallons.Text = "";
+
+                //we are lazy so the program puts the cursor back into focus after the message
+                txtMiles.Focus();
+                
+
+            }//end of catch statement
+
+        }//end of Calculate_Click
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }//end main
+}//end namespace
