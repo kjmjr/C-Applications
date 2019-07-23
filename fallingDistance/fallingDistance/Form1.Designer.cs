b12c14f3ada7b401cbdec13f7bff4e9ce130a228
Kevin McAdoo <53047422+kjmjr@users.noreply.github.com>
2019-07-23 19:00:39 -0400

    Falling Distance

---
+++ fallingDistance/fallingDistance/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace fallingDistance
+{
+    partial class frmFallingDistance
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.lblFallingDistance = new System.Windows.Forms.Label();
+            this.lblForTime = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(320, 295);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(156, 23);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // lblFallingDistance
+            // 
+            this.lblFallingDistance.AutoSize = true;
+            this.lblFallingDistance.Location = new System.Drawing.Point(285, 40);
+            this.lblFallingDistance.Name = "lblFallingDistance";
+            this.lblFallingDistance.Size = new System.Drawing.Size(209, 13);
+            this.lblFallingDistance.TabIndex = 1;
+            this.lblFallingDistance.Text = "Welcome to the Falling Distance Converter";
+            // 
+            // lblForTime
+            // 
+            this.lblForTime.AutoSize = true;
+            this.lblForTime.Location = new System.Drawing.Point(151, 109);
+            this.lblForTime.Name = "lblForTime";
+            this.lblForTime.Size = new System.Drawing.Size(155, 13);
+            this.lblForTime.TabIndex = 2;
+            this.lblForTime.Text = "Please enter a time into the box";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(320, 106);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(156, 20);
+            this.txtTime.TabIndex = 3;
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(320, 375);
+            this.txtResults.Name = "txtResults";
+            this.txtResults.Size = new System.Drawing.Size(156, 20);
+            this.txtResults.TabIndex = 4;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(151, 375);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(73, 13);
+            this.lblAnswer.TabIndex = 5;
+            this.lblAnswer.Text = "The answer is";
+            // 
+            // frmFallingDistance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.lblForTime);
+            this.Controls.Add(this.lblFallingDistance);
+            this.Controls.Add(this.btnCalc);
+            this.Name = "frmFallingDistance";
+            this.Text = "Falling Distance";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label lblFallingDistance;
+        private System.Windows.Forms.Label lblForTime;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ fallingDistance/fallingDistance/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//notes:
+//d = 1/2 * gt^2
+//d = distance in meters
+//g = 9.8
+//t is user input
+
+//create a method that accepts the argument of time that user inputs and converts 
+//it to meters when returning it 
+
+namespace fallingDistance
+{
+    public partial class frmFallingDistance : Form
+    {
+        public frmFallingDistance()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            double time = double.Parse(txtTime.Text);
+
+            double meters = FallingDistance(time);
+
+            //prints the results in the text box
+            txtResults.Text = meters.ToString("n2");
+
+        }//end btnCalc method
+
+        //our method that returns a double
+        private double FallingDistance(double time)
+        {
+            //d = 1/2 * gt^2
+            //d = distance in meters
+            //g = 9.8
+            //t is user input
+    
+            //variable for gravity
+            double g = 9.8;
+
+            double result = Math.Pow(time, 2.0);
+
+            double d = 1.0 / 2.0 * g * result;
+
+            return d;
+
+        }//end of Falling Distance method that returns the distance
+
+    }//end class
+}//end namespace
